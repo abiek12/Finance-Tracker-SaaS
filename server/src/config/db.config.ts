43d11248dfca27682cb480b50ce212063e3ac5a9
Abhishek p p <abhishekkanichery@gmail.com>
2024-11-18 22:51:22 +0530

    added winston logger for logging

--- server/src/config/db.config.ts
+++ server/src/config/db.config.ts
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
+import logger from "../utils/logger.utils";
 
 export const connectDB = async () => {
     try {        
         await mongoose.connect(process.env.MONGO_URI!);
-        console.log("Database connected successfully!");
+        logger.info("Database connected successfully!")
     } catch (error) {
-        console.log(error);
-        process.exit(1);
+        logger.error("Database connection failed!", error);
+        process.exit(1);;
     }
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,19 +1,18 @@
 import app from "./app";
 import { connectDB } from "./config/db.config";
+import logger from "./utils/logger.utils";
 
 const PORT = process.env.PORT || 4006;
 
 const start = async () => {
    try {
-    app.listen(PORT, async () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
-
-    // Connect to database
-    await connectDB();
-    
+        app.listen(PORT, async () => {
+            logger.info(`Server is running on port ${PORT}`);
+        });
+        // Connect to database
+        await connectDB();
    } catch (error) {
-        console.log(error);
+        logger.error("Server startup failed!", error);
         process.exit(1);
    }
 }
