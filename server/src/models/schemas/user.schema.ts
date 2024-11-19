f8abf2eb1a279688d94df5f6024b857b5ba3e055
Abhishek p p <abhishekkanichery@gmail.com>
2024-11-19 08:25:41 +0530

    added user schema and its interface

---
+++ server/src/models/schemas/user.schema.ts
@@ -0,0 +1,42 @@
+import mongoose, { Schema } from "mongoose";
+import { IUser } from "../interfaces/user.interface";
+
+const userSchema: Schema = new Schema({
+    userName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    isEmailVerified: {
+        type: Boolean,
+        default: false
+    },
+    twoFA: {
+        type: Boolean,
+        default: false
+    },
+    otp: {
+        type: String
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+export const User = mongoose.model<IUser & Document>('User', userSchema);
