8758ef0b2312aadf0fd512eb91d3c49fa0ba0258
Abhishek p p <abhishekkanichery@gmail.com>
2024-11-19 08:41:06 +0530

    added crud for user in the user repo

---
+++ server/src/models/repositories/user.repository.ts
@@ -0,0 +1,28 @@
+import { IUser } from "../interfaces/user.interface";
+import { User } from "../schemas/user.schema";
+
+export class UserRepository {
+    createUser = async (userDetails: IUser): Promise<IUser> => {
+        return await new User(userDetails).save();
+    }
+
+    findUserByEmail = async (email: string): Promise<IUser | null> => {
+        return await User.findOne({
+            email
+        });
+    }
+
+    findUserById = async (id: string): Promise<IUser | null> => {
+        return await User.findById(id);
+    }
+
+    updateUserById = async (id: string, userDetails: IUser): Promise<IUser | null> => {
+        return await User.findByIdAndUpdate(id, userDetails, {
+            new: true
+        });
+    }
+
+    deleteUserById = async (id: string): Promise<IUser | null> => {
+        return await User.findByIdAndDelete(id);
+    }
+}
