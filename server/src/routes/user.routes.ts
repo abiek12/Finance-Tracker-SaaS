55e72f49af481640ba1a3c952fedb14cc31b0e81
Abhishek p p <abhishekkanichery@gmail.com>
2024-11-23 10:29:09 +0530

    added the verify token middleware in the user profile management apis

--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { UserControllers } from "../controllers/user.controller";
+import { verifyToken } from "../middlewares/auth.middleware";
 
 const userRoutes = Router();
 const userController = new UserControllers();
 
 // Get user details
-userRoutes.get("/", userController.getUserDetails);
+userRoutes.get("/", verifyToken, userController.getUserDetails);
 // Update user details
-userRoutes.patch("/", userController.updateUserDetails);
+userRoutes.patch("/", verifyToken, userController.updateUserDetails);
 
 export default userRoutes;
