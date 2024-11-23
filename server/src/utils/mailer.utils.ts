e4c699928ff19055872358a4be3f2ce30317079c
Abhishek p p <abhishekkanichery@gmail.com>
2024-11-23 22:31:12 +0530

    added fucntion for render and replace the user name and verification link in the ejs template

--- server/src/utils/mailer.utils.ts
+++ server/src/utils/mailer.utils.ts
@@ -1,5 +1,7 @@
 import nodemailer from 'nodemailer';
 import logger from './logger.utils';
+import ejs from 'ejs';
+import path from 'path';
 
 // Create a transport object
 const transporter = nodemailer.createTransport({
@@ -12,14 +14,33 @@ const transporter = nodemailer.createTransport({
     }
 });
 
-// Send mail function
-export const sendMail = async (to: string, subject: string, text: string, html: string) => {
+// Render Ejs template
+export const renderTemplate = (templateName: string, data: any) => {
     try {
+        return new Promise((resolve, reject) => {
+            ejs.renderFile(path.join(__dirname, '../templates', templateName), data, (err, html) => {
+                if(err) {
+                    logger.error(`EJS TEMPLTE RENDERING:: Error rendering template: ${err}`);
+                    reject(err);
+                }
+                resolve(html);
+            });
+        });
+    } catch (error) {
+        logger.error(`EJS TEMPLTE RENDERING:: Error rendering template: ${error}`);
+        throw error;
+    }
+}
+
+// Send mail function
+export const sendMail = async (to: string, subject: string, templateName: string, data: any) => {
+    try {
+        const html = await renderTemplate(templateName, data) as string;
+        
         const mailOptions = {
             from: process.env.MAIL_USER,
             to,
             subject,
-            text,
             html
         };
 
