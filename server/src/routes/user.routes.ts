8ac66fc2a5eae048568ff591ba0b7d9851676915
Abhishek p p <abhishekkanichery@gmail.com>
2024-11-24 10:09:54 +0530

    added routes for send verification mail

--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -9,5 +9,7 @@ const userController = new UserControllers();
 userRoutes.get("/", verifyToken, userController.getUserDetails);
 // Update user details
 userRoutes.patch("/", verifyToken, userController.updateUserDetails);
+// Send verification email
+userRoutes.get("/send-verification-email", verifyToken, userController.sendVerificationEmail);
 
 export default userRoutes;
