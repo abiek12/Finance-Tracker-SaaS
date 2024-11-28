a71c4bedc6829361417056e25d62c6ee3de6caaf
Abhishek p p <abhishekkanichery@gmail.com>
2024-11-28 23:41:01 +0530

    give proper uploaded link to access

--- server/src/controllers/file.controller.ts
+++ server/src/controllers/file.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { INTERNAL_ERROR, SUCCESS } from '../utils/common.utils';
 import { errorResponse, successResponse } from '../utils/responseHandler.utils';
+import logger from '../utils/logger.utils';
 
 export class FileController {
     // Upload file
@@ -10,7 +11,11 @@ export class FileController {
                 res.status(INTERNAL_ERROR).send(errorResponse(INTERNAL_ERROR, "No file uploaded"));
                 return;
             }
-            res.status(SUCCESS).send(successResponse(SUCCESS, "File uploaded successfully", req.file.filename))
+            // Generate file URL
+            const fileUrl = `${req.protocol}://${req.get("host")}/uploads/${req.file.filename}`;
+
+            logger.info("FILE-CONTROLLER:: File uploaded successfully");
+            res.status(SUCCESS).send(successResponse(SUCCESS, "File uploaded successfully", fileUrl))
         } catch (error) {
             console.error("FILE-CONTROLLER:: Error occurred while uploading file", error);
             res.status(INTERNAL_ERROR).send(errorResponse(INTERNAL_ERROR, "Error occurred while uploading file"));
